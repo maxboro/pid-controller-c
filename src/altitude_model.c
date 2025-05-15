732b85265542c8cb318fa78a46f2e1f8df1a9ebf
Max B. <maksym_borovyk@ukr.net>
2025-05-15 18:52:12 +0300

    Unify names for state and params pointers

--- src/altitude_model.c
+++ src/altitude_model.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 void update_altitude(
-        const struct SimParams* sim_params_ptr, 
-        const struct AircraftParams* aircraft_params_ptr,
-        struct AircraftState* aircraft_state_ptr,
+        const struct SimParams* params_sim_ptr, 
+        const struct AircraftParams* params_aircraft_ptr,
+        struct AircraftState* state_aircraft_ptr,
         const double pid_target_thrust){
     // Check if PID calculated desired thrust is achievable by particular aircraft
-    aircraft_state_ptr->thrust = cap_value(pid_target_thrust, aircraft_params_ptr->min_thrust, aircraft_params_ptr->max_thrust);
+    state_aircraft_ptr->thrust = cap_value(pid_target_thrust, params_aircraft_ptr->min_thrust, params_aircraft_ptr->max_thrust);
 
     // Physics → Calculate net force
-    const double abs_velocity = aircraft_state_ptr->velocity > 0 ? aircraft_state_ptr->velocity : -aircraft_state_ptr->velocity;
-    const double gravity_force = aircraft_params_ptr->mass * sim_params_ptr->gravity;
-    const double drag_force = sim_params_ptr->drag_coefficient * aircraft_state_ptr->velocity * abs_velocity;
-    const double net_force = aircraft_state_ptr->thrust - gravity_force - drag_force;
+    const double abs_velocity = state_aircraft_ptr->velocity > 0 ? state_aircraft_ptr->velocity : -state_aircraft_ptr->velocity;
+    const double gravity_force = params_aircraft_ptr->mass * params_sim_ptr->gravity;
+    const double drag_force = params_sim_ptr->drag_coefficient * state_aircraft_ptr->velocity * abs_velocity;
+    const double net_force = state_aircraft_ptr->thrust - gravity_force - drag_force;
 
-    if (sim_params_ptr->verbose){
-        printf("aircraft_state_ptr->thrust %f\n", aircraft_state_ptr->thrust);
+    if (params_sim_ptr->verbose){
+        printf("state_aircraft_ptr->thrust %f\n", state_aircraft_ptr->thrust);
         printf("gravity_force %f\n", gravity_force);
         printf("abs_velocity %f\n", abs_velocity);
         printf("drag_force %f\n", drag_force);
@@ -23,17 +23,17 @@ void update_altitude(
     }
 
     // Update aircraft state
-    aircraft_state_ptr->acceleration = net_force / aircraft_params_ptr->mass;
-    aircraft_state_ptr->velocity += aircraft_state_ptr->acceleration * sim_params_ptr->dt;
-    aircraft_state_ptr->altitude += aircraft_state_ptr->velocity * sim_params_ptr->dt;
-    if (sim_params_ptr->verbose){
-        printf("aircraft_state_ptr->acceleration %f\n", aircraft_state_ptr->acceleration);
-        printf("aircraft_state_ptr->velocity %f\n", aircraft_state_ptr->velocity);
+    state_aircraft_ptr->acceleration = net_force / params_aircraft_ptr->mass;
+    state_aircraft_ptr->velocity += state_aircraft_ptr->acceleration * params_sim_ptr->dt;
+    state_aircraft_ptr->altitude += state_aircraft_ptr->velocity * params_sim_ptr->dt;
+    if (params_sim_ptr->verbose){
+        printf("state_aircraft_ptr->acceleration %f\n", state_aircraft_ptr->acceleration);
+        printf("state_aircraft_ptr->velocity %f\n", state_aircraft_ptr->velocity);
     }
-    if (aircraft_state_ptr->altitude < 0){
-        aircraft_state_ptr->altitude = 0;
-        if (aircraft_state_ptr->velocity < 0){
-            aircraft_state_ptr->velocity = 0;
+    if (state_aircraft_ptr->altitude < 0){
+        state_aircraft_ptr->altitude = 0;
+        if (state_aircraft_ptr->velocity < 0){
+            state_aircraft_ptr->velocity = 0;
         }
     }
 }
--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ void sim_loop(
         const struct SimParams* params_sim_ptr,
         const struct PIDParams* params_pid_ptr, 
         const struct AircraftParams* params_aircraft_ptr,
-        struct AircraftState* aircraft_state_ptr,
+        struct AircraftState* state_aircraft_ptr,
         struct PIDState* state_pid_ptr
         ){
     double pid_target_thrust;
@@ -19,16 +19,16 @@ void sim_loop(
         pid_target_thrust = compute_thrust(
             state_pid_ptr, params_pid_ptr, 
             params_sim_ptr->target_altitude, 
-            aircraft_state_ptr->altitude, params_sim_ptr->dt
+            state_aircraft_ptr->altitude, params_sim_ptr->dt
         );
         update_altitude(
             params_sim_ptr, params_aircraft_ptr, 
-            aircraft_state_ptr, pid_target_thrust
+            state_aircraft_ptr, pid_target_thrust
         );
 
         // print status
         printf("Thrust (PID): %f  Thrust (real): %f  Alt: %f\n", 
-            pid_target_thrust, aircraft_state_ptr->thrust, aircraft_state_ptr->altitude);
+            pid_target_thrust, state_aircraft_ptr->thrust, state_aircraft_ptr->altitude);
     }
 }
 
